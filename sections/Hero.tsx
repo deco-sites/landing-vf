e1719d9eca9f759e1a39f7072504783134d40ef0
Valter Filho <valterfilhobbmp@gmail.com>
2025-07-07 14:32:40 +0000

    Publish 6 from valter into origin/main

--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -6,6 +6,7 @@ export interface CTA {
   href: string;
   text: string;
   outline?: boolean;
+  size?: "xs" | "sm" | "md" | "lg";
 }
 
 export interface Props {
@@ -84,8 +85,8 @@ export default function HeroFlats({
                   id={item?.id}
                   href={item?.href}
                   target={item?.href.includes("http") ? "_blank" : "_self"}
-                  class={`font-normal btn btn-primary ${
-                    item.outline && "btn-outline"
+                  class={`font-normal btn btn-primary ${item.outline && "btn-outline"} ${
+                    item.size && `btn-${item.size}`
                   }`}
                 >
                   {item?.text}
