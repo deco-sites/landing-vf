ee4081c5eaed99992b7723d3fb7523ba76cb071a
Valter Filho <valter.filho@monks.com>
2025-07-08 15:58:42 -0300

    Adcionado os arquivos gerados pelo db.

--- db/schema.ts
+++ db/schema.ts
@@ -5,7 +5,3 @@ export const profiles = sqliteTable("profiles", {
   name: text("name"),
   email: text("email"),
 });
-
-
-  $env:DATABASE_URL="libsql://landing-vf-decocx.turso.io"
-  $env:DATABASE_AUTH_TOKEN="eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJleHAiOjE3NTIwMDAwNTcsImlhdCI6MTc1MTk5OTQ1NywiaWQiOiIxNTYxNDNhOC1jMGJkLTQ2M2EtOGFmMS0zZWE5NDc3YzBmYWYifQ.R2GE85YfpVq21VhzVKl9gKLL0c-3XtbYnal_MmVa_C6I_jMhJ2KP3kvx9em0BGK1GtQxSnYw-PO7Qj3EtSkDCw"
